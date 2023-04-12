df7292f97f4dff858f8b09556aef52e076c7199b
MarceloKade <98852912+MarceloKade@users.noreply.github.com>
2023-04-12 18:54:24 -0300

    Declarando constantes no arquivo script.js para capturar os elementos do HTML pelo id e classe correspondentes, permitindo a manipulação dos mesmos através do JavaScript. Foram capturados o elemento text-area, os botões de criptografia e descriptografia e o aside que irá exibir a mensagem criptografada ou descriptografada.

---
+++ script.js
@@ -0,0 +1,4 @@
+const textArea = document.getElementById('text-area');
+const criptografiaButton = document.getElementById('criptografar');
+const descriptografiaButton = document.getElementById('descriptografar');
+const mensagemAside = document.querySelector('.mensagem-criptografada');
