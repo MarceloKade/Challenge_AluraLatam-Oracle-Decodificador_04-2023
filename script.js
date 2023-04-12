67d19d8a4f94431eb197147e81fe218125b5b5c8
MarceloKade <98852912+MarceloKade@users.noreply.github.com>
2023-04-12 19:16:44 -0300

    Adicionado função criarTextoarea() para criar um elemento <textarea> para exibir o texto criptografado ou descriptografado dentro da tag aside. Essa função cria um novo elemento <textarea> com a classe 'mensagem-textarea', nomeado de textoCriptografado, adicionando-o ao corpo do documento e retornando-o.

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const textArea = document.getElementById('text-area');
 const criptografiaButton = document.getElementById('criptografar');
 const descriptografiaButton = document.getElementById('descriptografar');
 const mensagemAside = document.querySelector('.mensagem-criptografada');
+
+function criarTextoarea() {
+    const textoCriptografado = document.createElement('textarea');
+    textoCriptografado.classList.add('mensagem-textarea');
+    document.body.appendChild(textoCriptografado);
+
+    return textoCriptografado;
+}
